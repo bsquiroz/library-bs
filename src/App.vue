cc0ce3aa3068e4b5763d07f10b05605eaf2331e6
bsquiroz <bemq1605@gmail.com>
2024-04-30 18:24:21 -0500

    project base

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import Layout from "./components/Layout/Layout.vue";
+</script>
+
+<template>
+  <Layout>
+    <h2 class="text-2xl">Hola <span class="text-primary">mundo</span></h2>
+    <Button>Click me</Button>
+  </Layout>
+</template>
